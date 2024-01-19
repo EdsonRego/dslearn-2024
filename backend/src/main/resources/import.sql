b8e8705de15d4d1ed85492213096d324209d80e2
EdsonRego <edsonxrego@gmail.com>
2024-01-18 21:18:31 -0300

    Entity - Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,6 +13,4 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_category (name, created_At) VALUES ('Livros', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Eletrônicos', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Computadores', NOW());
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://live.staticflickr.com/7235/7260921750_3e19f6472d_b.jpg','https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
