c2e035ac376b46c61043d0bad5a8cdf961c8859e
EdsonRego <edsonxrego@gmail.com>
2024-01-19 17:42:57 -0300

    Section entity implementation

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +29,9 @@ public class Offer implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
 	
 	public Offer() {
 	}
@@ -78,13 +84,33 @@ public class Offer implements Serializable{
 	public void setCourse(Course course) {
 		this.course = course;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	
+	public List<Resource> getResources() {
+		return resources;
 	}
 
 	@Override
-	public String toString() {
-		return "Offer [id=" + id + "]";
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Offer other = (Offer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
