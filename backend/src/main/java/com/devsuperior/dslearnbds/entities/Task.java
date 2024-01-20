3de25459666efd6ec425b36bce85f3802430536f
EdsonRego <edsonxrego@gmail.com>
2024-01-19 23:13:46 -0300

    Lessons seed, fix LessonsDone

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Task.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Task.java
@@ -8,25 +8,25 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_task")
-public class Task extends Lesson{
+public class Task extends Lesson {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String description;
-	private Integer questionsCount;
+	private Integer questionCount;
 	private Integer approvalCount;
 	private Double weight;
 	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")	
 	private Instant dueDate;
 	
 	public Task() {
 	}
 
-	public Task(Long id, String title, Integer position, Section section, String description, Integer questionsCount,
+	public Task(Long id, String title, Integer position, Section section, String description, Integer questionCount,
 			Integer approvalCount, Double weight, Instant dueDate) {
 		super(id, title, position, section);
 		this.description = description;
-		this.questionsCount = questionsCount;
+		this.questionCount = questionCount;
 		this.approvalCount = approvalCount;
 		this.weight = weight;
 		this.dueDate = dueDate;
@@ -40,12 +40,12 @@ public class Task extends Lesson{
 		this.description = description;
 	}
 
-	public Integer getQuestionsCount() {
-		return questionsCount;
+	public Integer getQuestionCount() {
+		return questionCount;
 	}
 
-	public void setQuestionsCount(Integer questionsCount) {
-		this.questionsCount = questionsCount;
+	public void setQuestionCount(Integer questionCount) {
+		this.questionCount = questionCount;
 	}
 
 	public Integer getApprovalCount() {
